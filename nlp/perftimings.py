f6956f5efe71d3397ac663dd6ab0c2a9ce9886fd
Matt Jones <matt-biz@mhjones.org>
2008-09-06 13:31:09 -0700

    Performance timings for numpy.array (fast for math ops, slow for iteration / initialization)

--- nlp/perftimings.py
+++ nlp/perftimings.py
@@ -8,6 +8,11 @@ from counter import Counter
 from countermap import CounterMap
 from nlp import counter
 
+from numpy.core import array as np_array
+from numpy.core import divide
+
+test_iteration = False
+
 def main():
 	key_src = range(10000)
 	iter_src = array('i')
@@ -41,70 +46,80 @@ def main():
 	last = time.time()
 	cCnt = cCounter_init(iter_src)
 	print "%s: %f" % ("cCounter", time.time()-last)
-
-	#### Random access
-	print "Random Access"
-	rand_access_src = array('i')
-	for i in xrange(10000000):
-		key = random.choice(key_src)
-		rand_access_src.append(key)
-
 	last = time.time()
+	npa = npa_init(iter_src)
+	print "%s: %f" % ("npa", time.time()-last)
 
-	for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt")):
-		rand_access(container, rand_access_src)
-		print "%s: %f" % (name, time.time()-last)
-		last = time.time()
+	if test_iteration:
 
-	#### Forward Iteration
-	print "Iteration Access"
-	last = time.time()
+#### Random access
+			print "Random Access"
+			rand_access_src = array('i')
+			for i in xrange(10000000):
+				key = random.choice(key_src)
+				rand_access_src.append(key)
 
-	for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt")):
-		iter_access(container, 1000, 'values' in dir(container))
-		print "%s: %f" % (name, time.time()-last)
-		last = time.time()
+				last = time.time()
 
-	#### Random-stride forward iteration (simulates sorted sparse index)
-	print "Sparse Iteration Access"
-	stride_src = sorted(rand_access_src)
-	last = time.time()
+			for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt, npa), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt", "npa")):
+				rand_access(container, rand_access_src)
+				print "%s: %f" % (name, time.time()-last)
+				last = time.time()
 
-	for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt")):
-		for pos in stride_src:
-			temp = container[pos]
-		print "%s: %f" % (name, time.time()-last)
-		last = time.time()
+			#### Forward Iteration
+			print "Iteration Access"
+			last = time.time()
 
-	#### Random-stride backward-forward iteration (simulates sorted sparse index)
-	print "Sparse Forward/Backward Iteration Access"
-	back = list(xrange(len(stride_src)))
-	for i in xrange(len(stride_src)):
-		back[i] = random.choice([True,False])
+			for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt, npa), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt", "npa")):
+				iter_access(container, 1000, 'values' in dir(container))
+				print "%s: %f" % (name, time.time()-last)
+				last = time.time()
 
-	access_order = list()
-	current_pos = 0
-	for (pos, stride) in enumerate(stride_src):
-		if back[pos]: current_pos = (current_pos - stride) % len(container)
-		else: current_pos = (current_pos + stride) % len(container)
-		access_order.append(current_pos)
+			#### Random-stride forward iteration (simulates sorted sparse index)
+			print "Sparse Iteration Access"
+			stride_src = sorted(rand_access_src)
+			last = time.time()
 
-	last = time.time()
+			for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt, npa), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt", "npa")):
+				for pos in stride_src:
+					temp = container[pos]
+				print "%s: %f" % (name, time.time()-last)
+				last = time.time()
 
-	for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt")):
-		for pos in access_order:
-			temp = container[pos]
-		print "%s: %f" % (name, time.time()-last)
-		last = time.time()
+			#### Random-stride backward-forward iteration (simulates sorted sparse index)
+			print "Sparse Forward/Backward Iteration Access"
+			back = list(xrange(len(stride_src)))
+			for i in xrange(len(stride_src)):
+				back[i] = random.choice([True,False])
+
+			access_order = list()
+			current_pos = 0
+			for (pos, stride) in enumerate(stride_src):
+				if back[pos]: current_pos = (current_pos - stride) % len(container)
+				else: current_pos = (current_pos + stride) % len(container)
+				access_order.append(current_pos)
+
+			last = time.time()
+
+			for (container, name) in zip((td, tdd, tl, tda, tla, cnt, cCnt, npa), ("td", "tdd", "tl", "tda", "tla", "cnt", "cCnt", "npa")):
+				for pos in access_order:
+					temp = container[pos]
+				print "%s: %f" % (name, time.time()-last)
+				last = time.time()
 		
 	#### Test counter-specifics
 	print "arg_max"
 	last = time.time()
 	
-	for (container, name) in zip((cnt, cCnt), ("cnt", "cCnt")):
+	for (container, name) in zip((cnt, cCnt, npa), ("cnt", "cCnt")):
 		temp = container.arg_max()
 		print "%s: %f (%s)" % (name, time.time()-last, temp)
 		last = time.time()
+
+	last = time.time()
+	temp = npa.argmax()
+	print "%s: %f (%s)" % ("npa", time.time()-last, temp)
+	last = time.time()
 	
 	print "total_count"
 	last = time.time()
@@ -113,7 +128,12 @@ def main():
 		temp = container.total_count()
 		print "%s: %f (%f)" % (name, time.time()-last, temp)
 		last = time.time()
-	
+
+	last = time.time()
+	temp = npa.sum()
+	print "%s: %f (%f)" % ("npa", time.time()-last, temp)
+	last = time.time()
+		
 	print "normalize"
 	last = time.time()
 
@@ -122,6 +142,11 @@ def main():
 		print "%s: %f" % (name, time.time()-last)
 		last = time.time()
 
+	last = time.time()
+	npa = divide(npa, npa.sum())
+	print "%s: %f" % ("npa", time.time()-last)
+	last = time.time()
+
 	print "arg_max"
 	last = time.time()
 
@@ -130,6 +155,11 @@ def main():
 		print "%s: %f (%s)" % (name, time.time()-last, temp)
 		last = time.time()
 
+	last = time.time()
+	temp = npa.argmax()
+	print "%s: %f (%s)" % ("npa", time.time()-last, temp)
+	last = time.time()
+		
 	print "total_count"
 	last = time.time()
 
@@ -137,6 +167,11 @@ def main():
 		temp = container.total_count()
 		print "%s: %f (%f)" % (name, time.time()-last, temp)
 		last = time.time()
+
+	last = time.time()
+	temp = npa.sum()
+	print "%s: %f (%f)" % ("npa", time.time()-last, temp)
+	last = time.time()
 
 def dict_init(iter_src):
 	print "dict"
@@ -192,6 +227,13 @@ def countermap_init(iter_src):
 	for i in iter_src:
 		test_countermap[i] += 1
 	return test_countermap
+
+def npa_init(iter_src):
+	test_numpy_array = np_array(list(itertools.repeat(0.0, 10000)))
+	# numpy_array
+	for i in iter_src:
+		test_numpy_array[i] += 1.0
+	return test_numpy_array
 
 def rand_access(container, iteration):
 	temp_value = 0.0
