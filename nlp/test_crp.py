4fecd07149a48148db07b2641e9346b995b5e8f7
Matt Jones <matt-biz@mhjones.org>
2009-04-11 14:27:27 -0700

    better tests

--- nlp/test_crp.py
+++ nlp/test_crp.py
@@ -16,9 +16,49 @@ class CRPGibbsSamplerTest(unittest.TestCase):
 		self.points[2]['x'] = 10.0
 		self.points[2]['y'] = 10.0
 
-		self.sampler = CRPGibbsSampler(self.points, mh_mean=Counter((('x', 5.0), ('y', 5.0))), mh_precision=1.0, cluster_precision=0.25)
+		self.sampler = CRPGibbsSampler(self.points)
 
-	def test_add_self(self):
+	def test_simple_sample_datum(self):
+		self.sampler._sample_datum = lambda datum: 0
+		self.sampler.log_likelihood = lambda: 0.0
+
+		self.sampler.gibbs(1)
+
+		self.assertTrue(all(v == 0 for v in self.sampler._datum_to_cluster.itervalues()))
+		self.assertTrue(all(len(c) == 0 for idx, c in self.sampler._cluster_to_datum.iteritems() if idx != 0), self.sampler._cluster_to_datum)
+		self.assertEqual(len(self.sampler._cluster_to_datum[0]), 2)
+
+		self.sampler.gibbs(1)
+
+		self.assertEqual(self.sampler._cluster_count[0], 1)
+		self.assertEqual(self.sampler._iteration_likelihoods[0], 0.0)
+
+	def test_two_cluster_sample_datum(self):
+		self.sampler._sample_datum = lambda datum: 0 if datum == self.points[1] else 1
+		self.sampler.log_likelihood = lambda: 0.0
+
+		self.sampler.gibbs(1)
+		self.assertEqual(self.sampler._datum_to_cluster[1], 0)
+		self.assertEqual(self.sampler._datum_to_cluster[2], 1)
+
+		self.assertEqual(len(self.sampler._cluster_to_datum[0]), 1)
+		self.assertEqual(len(self.sampler._cluster_to_datum[1]), 1)
+
+		# invert the sampler
+		self.sampler._sample_datum = lambda datum: 1 if datum == self.points[1] else 0
+		self.sampler.gibbs(1)
+
+		self.assertEqual(self.sampler._cluster_count[0], 2)
+		self.assertEqual(self.sampler._iteration_likelihoods[0], 0.0)
+
+		# Assert they switched clusters
+		self.assertEqual(self.sampler._datum_to_cluster[1], 1)
+		self.assertEqual(self.sampler._datum_to_cluster[2], 0)
+
+		self.assertEqual(len(self.sampler._cluster_to_datum[0]), 1)
+		self.assertEqual(len(self.sampler._cluster_to_datum[1]), 1)
+
+	def _test_add_self(self):
 		# Shouldn't be initialized yet...
 		self.assertEqual(self.sampler.log_likelihood(), 0.0)
 
