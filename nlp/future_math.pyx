56653fc2178d7350b7ae96379655bcf3df55e7fd
Matt Jones <matt-biz@mhjones.org>
2009-04-13 23:03:54 -0700

    didn't mean to do this

--- nlp/future_math.pyx
+++ nlp/future_math.pyx
@@ -3,7 +3,7 @@ include "math.pxi"
 DEF ROOT2 = 1.4142135623730951
 
 def g_cdf(double x, double mean, double variance):
-	return 0.5 + 0.5 * erf(abs(x - mean) / (variance * ROOT2))
+	return 0.5 + 0.5 * erf((x - mean) / (variance * ROOT2))
 
 def g_log_cdf(double x, double mean, double variance):
-	return log(0.5 + 0.5 * erf(abs(x-mean) / (variance * ROOT2)))
+	return log(0.5 + 0.5 * erf((x-mean) / (variance * ROOT2)))
