6c39e3db85c9624bf58b806a2c14f1ca176a6882
Matt Jones <matt-biz@mhjones.org>
2009-04-30 22:12:37 -0700

    log prob was totally broken

--- nlp/distributions.py
+++ nlp/distributions.py
@@ -9,15 +9,19 @@ class Gaussian(object):
 		prob = 1.0
 
 		for key, pt in point.iteritems():
-			prob *= g_cdf(pt+discretization, mean[key], precision[key]) - g_cdf(pt-discretization, mean[key], precision[key])
+			prob *= abs(g_cdf(pt+discretization, mean[key], precision[key]) - g_cdf(pt-discretization, mean[key], precision[key]))
 
 		return prob
 
 	@classmethod
 	def log_prob(cls, point, mean, precision, debug=False, discretization=0.00001):
-		prob = 0.0
+		log_prob = 0.0
 
+#		print "Looking at range %s => %s for mean %s" % (point-discretization, point+discretization, mean)
 		for key, pt in point.iteritems():
-			prob += g_log_cdf(pt+discretization, mean[key], precision[key]) - g_cdf(pt-discretization, mean[key], precision[key])
+			prob = abs(g_cdf(pt+discretization, mean[key], precision[key]) - g_cdf(pt-discretization, mean[key], precision[key]))
+#			print "prob: %f" % prob
+			log_prob += log(prob) if prob else float("-inf")
+#		print "log_prob: %f" % log_prob
 
-		return prob
+		return log_prob
