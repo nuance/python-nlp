3a0f7037b86abe0473d5a5b91daf6560c1bb1780
Matt Jones <matt-biz@mhjones.org>
2009-05-03 20:50:00 -0700

    fix future math

--- nlp/future_math.pyx
+++ nlp/future_math.pyx
@@ -2,8 +2,8 @@ include "math.pxi"
 
 DEF ROOT2 = 1.4142135623730951
 
-def g_cdf(double x, double mean, double variance):
-	return 0.5 + 0.5 * erf((x - mean) / (variance * ROOT2))
+def g_cdf(double x, double mean, double precision):
+	return 0.5 + 0.5 * erf((x - mean) * sqrt(precision) / ROOT2)
 
-def g_log_cdf(double x, double mean, double variance):
-	return log(0.5 + 0.5 * erf((x-mean) / (variance * ROOT2)))
+def g_log_cdf(double x, double mean, double precision):
+	return log(0.5 + 0.5 * erf((x-mean) * sqrt(precision) / ROOT2))
--- nlp/math.pxi
+++ nlp/math.pxi
@@ -2,3 +2,4 @@ cdef extern from "math.h":
 	double log(double x)
 	double erf(double x)
 	double abs(double x)
+	double sqrt(double x)
