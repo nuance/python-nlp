42f08f52ab02aefbaa0559afb92e341f55dd20a3
Matt Jones <matt-biz@mhjones.org>
2009-05-18 00:52:13 -0700

    add multivariate gaussian w/ covariance matrix, use scipy for approximating the cdf

--- nlp/distributions.py
+++ nlp/distributions.py
@@ -1,8 +1,11 @@
 from math import log
 
+from lib import mvncdf
 from future_math import g_cdf
 
 class Gaussian(object):
+	"""multivariate gaussian with independent components
+	"""
 	@classmethod
 	def prob(cls, point, mean, precision, debug=False, discretization=0.1):
 		prob = 1.0
@@ -23,3 +26,21 @@ class Gaussian(object):
 			log_prob += log(prob) if prob else float("-inf")
 
 		return log_prob
+
+class MultivariateGaussian(object):
+	"""like Gaussian, but allows dependent components
+	"""
+	@classmethod
+	def prob(cls, point, mean, covariance_matrix, debug=False, discretization=0.1):
+		keys, matrix = covariance_matrix.matrix()
+
+		lower = [point[key] - discretization for key in keys]
+		upper = [point[key] + discretization for key in keys]
+		mean = [mean[key] for key in keys]
+
+		return mvncdf.mvnormcdf(lower, upper, mean, matrix)
+
+	@classmethod
+	def log_prob(cls, point, mean, covariance_matrix, debug=False, discretization=0.1):
+		return log(cls.prob(point, mean, covariance_matrix, debug=debug, discretization=discretization))
+
